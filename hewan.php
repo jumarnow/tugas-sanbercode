0d733f00bcca5cf1b121d5e054b7682a1d74aa6a
jumarnow <jumarno.mwb@gmail.com>
2021-01-12 13:35:21 +0700

    Tugas 1 rev

--- hewan.php
+++ hewan.php
@@ -1,63 +1,109 @@
 <?php
 
-trait Hewan {
+abstract class Hewan {
     public $nama, $darah = 50, $jumlahKaki, $keahlian;
 
-    public function __construct($nama, $darah, $jumlahKaki, $keahlian){
+    public function __construct($nama){
         $this->nama = $nama;
-        $this->darah = $darah;
-        $this->jumlahKaki = $jumlahKaki;
-        $this->keahlian = $keahlian;
     }
     
     public function atraksi(){
-        echo $this->nama ." sedang ".$this->keahlian;
+        $str = $this->nama ." sedang ".$this->keahlian;
+        return $str;
+    }
+
+    abstract public function getInfoHewan();
+
+    public function getInfo(){
+        $str =  "Nama : $this->nama" . "<br>" . 
+                "Darah : $this->darah" . "<br>" . 
+                "Jumlah Kaki : $this->jumlahKaki" . "<br>" . 
+                "Keahlian : $this->keahlian" . "<br>" ;
+                return $str;
     }
 }
 
 trait Fight{
     public $attackPower, $defencePower;
 
-    public function __construct($attackPower, $defencePower){
-        $this->attackPower = $attackPower;
-        $this->defencePower = $defencePower;
+    // public function __construct($attackPower, $defencePower){
+    //     $this->attackPower = $attackPower;
+    //     $this->defencePower = $defencePower;
+    // }
+
+    public function serang($hewan){
+        echo "$this->nama sedang menyerang $hewan->nama" . "<br>";
+        echo "=============================================" . "<br>";
+        $hewan->diserang($this);
+    }
+    public function diserang($hewan){
+        echo "$this->nama sedang diserang $hewan->nama" . "<br>";
+        $this->darah = $this->darah - ($hewan->attackPower/$this->defencePower);
+
+        echo "Darah $this->nama tersisa : $this->darah". "<br>";
+    }
+    
+    
+    
+
+}
+
+class Elang extends Hewan{
+    use Fight;
+
+    public function __construct($nama){
+        parent::__construct($nama);
+        $this->jumlahKaki = 2;
+        $this->keahlian = "terbang tinggi";
+        $this->attackPower = 10;
+        $this->defencePower = 5;
+
     }
 
-    public function serang(){
-        echo $this->nama. ' sedang menyerang '. $elang;
-    }
-    public function diserang(){
-        echo $this->nama. ' sedang menyerang '. $elang;
-    }
     public function getInfoHewan(){
-        
+        $str = "=== ELANG ===" . "<br>" . 
+                parent::getInfo() .
+                "Attack Power : $this->attackPower" . "<br>" . 
+                "Defence Power : $this->defencePower" . "<br>" 
+                ;
+
+        return $str;
     }
-    
-    
+}
+
+class Harimau extends Hewan{
+    use Fight;
+
+    public function __construct($nama){
+        parent::__construct($nama);
+        $this->jumlahKaki = 4;
+        $this->keahlian = "lari cepat";
+        $this->attackPower = 7;
+        $this->defencePower = 8;
+
+    }
+
+    public function getInfoHewan(){
+        $str = "=== HARIMAU ===" . "<br>" . 
+                parent::getInfo() .
+                "Attack Power : $this->attackPower" . "<br>" . 
+                "Defence Power : $this->defencePower" . "<br>" 
+                ;
+                
+        return $str;
+    }
 
 }
 
-class Elang{
-    use Hewan, Fight;
-}
+$elang_1 = new Elang("Elang Jawa");
+$harimau_1 = new Harimau("Harimau Sumatra");
+echo $harimau_1->atraksi() . "<br>";
+echo $harimau_1->getInfoHewan() . "<br>";
+echo $elang_1->getInfoHewan() . "<br>";
 
-class Harimau{
-    use Hewan, Fight;
-}
+$elang_1->serang($harimau_1) . "<br>";
+echo $harimau_1->getInfoHewan() . "<br>";
+$harimau_1->serang($elang_1) . "<br>";
 
-$elang = new Elang("elang", 2, "terbang tinggi", 10, 5);
-// $elang->nama = "elang";
-// $elang->jumlahKaki = 2;
-// $elang->keahlian = "terbang tinggi";
-// $elang->attackPower = 10;
-// $elang->defencePower = 5;
-
-
-$harimau = new Harimau("harimau", 4, "lari cepat", 7, 8);
-// $elang->nama = "harimau";
-// $elang->jumlahKaki = 4;
-// $elang->keahlian = "lari cepat";
-// $elang->attackPower = 7;
-// $elang->defencePower = 8;
 
 ?>
